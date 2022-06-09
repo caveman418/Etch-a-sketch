d98ef23b09bae3c755188469f209f75f4748090e
matt <mattcave814@gmail.com>
2022-06-08 19:14:59 -0500

    Added event listener to each div and had it toggle classes for css styling

--- script.js
+++ script.js
@@ -6,20 +6,28 @@ const resizeButton = document.querySelector('.resize');
 output.textContent = slider.value;
 
 slider.addEventListener('input', () => {
-    output.textContent = slider.value;
-
+    output.textContent = slider.value; //when slider is moved, number underneath is updated
 });
 
-resizeButton.addEventListener('click', () => {
+function resize(e) {
     grid.textContent = ''; //clear grid
 
     for (let i = 1; i <= slider.value**2; i++) {
-
         const div = document.createElement('div');
         div.classList.add('box');
-        div.style.cssText = 'width: ' + (100/slider.value) + '%';
+        div.style.cssText = 'width: ' + (100/slider.value) + '%'; //makes n divs fit into 100% of the canvas (width is set to make flex wrap work)
 
-        div.setAttribute('id', 'id:' + i);
+        div.addEventListener('mousedown', (e) => { //adds event listener to every single div
+            e.target.classList.add('activated');
+        });
+        div.addEventListener('mouseenter', (e) => { 
+            e.target.classList.add('activated');
+        });
+        
         grid.appendChild(div);
     }
-})
+}
+
+resize(); //initialize 16x16 grid when page is loaded
+
+resizeButton.addEventListener('click', resize); //run resize function above when resize button is clicked
