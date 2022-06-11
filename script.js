aae20b44ae3e5ff8ad32c9da49d5fbd50407be7a
matt <mattcave814@gmail.com>
2022-06-10 21:03:05 -0500

    Made RGB mode and eraser mode into a button instead of checkboxes, made it so only one mode can be selected at a time (draw/erase/rgb), changed some layout/css

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let isPainting = false;
-let isRgb = false;
-let isEraser = false;
 
 const slider = document.getElementById('myRange');
 const output = document.querySelector('.sliderOutput');
 const grid = document.querySelector('.grid');
 const resizeButton = document.querySelector('.resize');
 const clearButton = document.querySelector('.clear');
-const rgbSwitch = document.querySelector('.checkrgb');
+const rgbButton = document.querySelector('.checkrgb');
 const eraserButton = document.querySelector('.eraser');
+const drawButton = document.querySelector('.drawmode');
+const allButtons = document.querySelectorAll('.button');
 
 output.textContent = `${slider.value} x ${slider.value}`; //initialize grid size number underneath slider (16)
 
@@ -16,13 +16,15 @@ slider.addEventListener('input', () => {
     output.textContent = `${slider.value} x ${slider.value}`; //when slider is moved, number underneath is updated
 });
 
-function draw(e) { //prioritizes eraser mode over rgb mode if both are activated
-    if (isEraser) {
-        e.target.style.backgroundColor = 'rgb(245,245,245)';
-    } else if (isRgb) {
-        e.target.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
-    } else {
+function draw(e) {
+    if (drawButton.classList.contains('activated')) {
         e.target.style.backgroundColor = document.querySelector('.colorpicker').value;
+    }
+    if (eraserButton.classList.contains('activated')) {
+        e.target.style.backgroundColor = 'rgb(249,251,255)';
+    }
+    if (rgbButton.classList.contains('activated')) {
+        e.target.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
     }
 };
 
@@ -33,7 +35,7 @@ function resize(e) { //clears grid and adds new divs with event listeners attach
         const div = document.createElement('div');
         div.classList.add('box');
         div.style.cssText = 'width: ' + (100/slider.value) + '%'; //makes n divs fit into 100% of the canvas (width is set to make flex wrap work)
-        div.style.backgroundColor = 'rgb(245, 245, 245)';
+        div.style.backgroundColor = 'rgb(249,251,255)';
 
         div.addEventListener('mousedown', (e) => { //add these following event listeners to each div as they're created
             isPainting = true;
@@ -52,12 +54,19 @@ function resize(e) { //clears grid and adds new divs with event listeners attach
     }
 }
 
+function toggleMode(e) {
+    allButtons.forEach(btn => btn.classList.remove('activated'));
+    e.target.classList.add('activated');
+}
+
 resize(); //initialize 16x16 grid when page is loaded
 
 resizeButton.addEventListener('click', resize); //run resize function above when resize button is clicked
 
 clearButton.addEventListener('click', resize); //the clear button does the exact same as the resize button
 
-rgbSwitch.addEventListener('click', (e) => {isRgb = !isRgb}); //toggles rgb mode
+drawButton.addEventListener('click', (e) => toggleMode(e)); //unactivates all buttons then activates draw
 
-eraserButton.addEventListener('click', (e) => {isEraser = !isEraser}); //toggles rgb mode
+rgbButton.addEventListener('click', (e) => toggleMode(e));
+
+eraserButton.addEventListener('click', (e) => toggleMode(e));
